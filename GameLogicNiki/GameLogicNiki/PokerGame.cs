60b97718320fe56cce9bf147dff87d75e1710c74
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-11-27 10:15:59 +0200

    Initial game structure - 2 smart players poker

---
+++ GameLogicNiki/GameLogicNiki/PokerGame.cs
@@ -0,0 +1,21 @@
+﻿namespace GameLogicNiki
+{
+    using System;
+
+    using TexasHoldem.Logic.GameMechanics;
+
+    public class PokerGame
+    {
+        public static void Main()
+        {
+            var player1 = new SmartPlayer();
+            var player2 = new SmartPlayer();
+
+            var game = new TwoPlayersTexasHoldemGame(player1, player2);
+
+            Console.WriteLine("Winner is: {0}", game.Start().Name);
+            
+            Console.WriteLine("Rounds played: {0}", game.HandsPlayed);
+        }
+    }
+}
